38cf694bbbed60c83ce51a6240323cc3e31931bf
navneet kaur batth <navneetbatth1989@gmail.com>
2019-06-07 14:31:47 +1000

    test new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Testing Git Dev</h1>
+        <h1>Testing Git Dev New branch</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
